b736e9191183bb8783766817b6b277c16294f92d
Pingu <pingu2k4@hotmail.co.uk>
2021-10-24 19:13:47 +0100

    Added Telerik to Blazor Project

--- TelerikBlazorGrid-Dapper/TelerikBlazorGrid-Dapper.Blazor/Startup.cs
+++ TelerikBlazorGrid-Dapper/TelerikBlazorGrid-Dapper.Blazor/Startup.cs
@@ -29,6 +29,8 @@ namespace TelerikBlazorGrid_Dapper.Blazor
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
+            services.AddTelerikBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
